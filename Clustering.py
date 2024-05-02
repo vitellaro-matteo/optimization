79490cda2ce5cc9db9596b157a324e991f08c6ae
Faraz Mogal <faraz.mogal@gmail.com>
2024-05-02 21:57:49 +0200

    created return_solo_clusterhead() function which performs the task of choosing just one clusterhead if a UAV belongs to more than one cluster. The choice taken into consdieration is: Lowest ID clusterhead is chosen if UAV is part of different clusters.

--- Clustering.py
+++ Clustering.py
@@ -142,6 +142,39 @@ def clusterTraversal(node, k, visited, visited_nodes):
         if neighbor.weight == node.weight - 1 and neighbor.id not in visited:
             clusterTraversal(neighbor,k, visited, visited_nodes)
 
+
+def return_solo_clusterhead(clusters):
+    
+    smallest_keys = {}
+
+    # Iterate over the dictionary items
+    for key, value in clusters.items():
+        # Iterate over the values
+        for item in value:
+            # If the item is already present in smallest_keys and the current key is smaller, update it
+            if item in smallest_keys and key < smallest_keys[item]:
+                smallest_keys[item] = key
+            # If the item is not present in smallest_keys, add it with the current key
+            elif item not in smallest_keys:
+                smallest_keys[item] = key
+
+    print("smallest_keys:", smallest_keys)
+    # Create a dictionary to store the result
+    result = {}
+
+    # Iterate over the dictionary items
+    for key, value in clusters.items():
+        # Iterate over the values
+        for item in value:
+            # If the smallest key for the current item matches the current key, add it to the result
+            if smallest_keys[item] == key:
+                if key not in result:
+                    result[key] = []
+                result[key].append(item)
+
+    print("result after resolving multiclusterhead issue", result)
+
+
 # Generate a list of UAV objects
 def main():
     num_uavs = 25
@@ -167,9 +200,13 @@ def main():
     print("Final Clusters:")
     print(finalClusters)
 
+    finalClustersAfterRemovingDuplicateClusterheads = return_solo_clusterhead(finalClusters)
+
     uavGraph(clustered_nodes,1,k)
 
-    return clustered_nodes,finalClusters
+
+
+    return clustered_nodes,finalClusters,finalClustersAfterRemovingDuplicateClusterheads
 
 if __name__ == "__main__":
     main()
